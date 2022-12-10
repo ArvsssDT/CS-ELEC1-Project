895785b3ee68061bb53bb4ebe16b9ce729a1e386
ArvsssDT <82487903+ArvsssDT@users.noreply.github.com>
2022-12-10 22:04:17 +0800

    Add files via upload

--- iris_plot.py
+++ iris_plot.py
@@ -11,17 +11,11 @@ from sklearn.preprocessing import Normalizer
 colnames=["SepalLengthCm", "SepalWidthCm","PetalLengthCm","PetalWidthCm", "Species","Id"]
 dataset = pd.read_csv("iris.data", header = None, names= colnames )
 
-# (150,6)
-# dataset.shape 
-# dataset.head(5)
-# dataset.describe()
-
 dataset.groupby('Species').size()
 
 feature_columns = ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm','PetalWidthCm']
 X = dataset[feature_columns].values
 y = dataset['Species'].values
-
 
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
@@ -35,10 +29,10 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, rando
 
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 # only use when using jupyter
 #%matplotlib inline
 
+# =========================== PARALLEL COORDINATES PLOT ===========================
 from pandas.plotting import parallel_coordinates
 plt.figure(figsize=(15,10))
 parallel_coordinates(dataset.drop("Id", axis=1), "Species")
@@ -48,6 +42,7 @@ plt.ylabel('Features values', fontsize=15)
 plt.legend(loc=1, prop={'size': 15}, frameon=True,shadow=True, facecolor="white", edgecolor="black")
 plt.show()
 
+# =========================== ANDREWS CURVES ===========================
 from pandas.plotting import andrews_curves
 plt.figure(figsize=(15,10))
 andrews_curves(dataset.drop("Id", axis=1), "Species")
@@ -55,14 +50,12 @@ plt.title('Andrews Curves Plot', fontsize=20, fontweight='bold')
 plt.legend(loc=1, prop={'size': 15}, frameon=True,shadow=True, facecolor="white", edgecolor="black")
 plt.show()
 
+# =========================== PAIR PLOT DIAGRAM ===========================
 plt.figure()
 sns.pairplot(dataset.drop("Id", axis=1), hue = "Species", size=3, markers=["o", "s", "D"])
-# plt.show()
+plt.show()
 
-plt.figure()
-dataset.drop("Id", axis=1).boxplot(by="Species", figsize=(15, 10))
-# plt.show()
-
+# =========================== 3D AXES DIAGRAM ===========================
 from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure(1, figsize=(20, 15))
 ax = Axes3D(fig, elev=48, azim=134)
@@ -83,57 +76,4 @@ ax.set_ylabel("Sepal Width [cm]", fontsize=25)
 ax.yaxis.set_ticklabels([])
 ax.set_zlabel("Petal Length [cm]", fontsize=25)
 ax.zaxis.set_ticklabels([])
-
-# plt.show()
-
-# =========================== FOR MAKING PREDICTIONS ===========================
-
-# Fitting clasifier to the Training set
-# Loading libraries
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.model_selection import cross_val_score
-
-# Instantiate learning model (k = 3)
-classifier = KNeighborsClassifier(n_neighbors=3)
-
-# Fitting the model
-classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-
-cm = confusion_matrix(y_test, y_pred)
-cm
-
-accuracy = accuracy_score(y_test, y_pred)*100
-print('Accuracy of our model is equal ' + str(round(accuracy, 2)) + ' %.')
-
-# creating list of K for KNN
-k_list = list(range(1,50,2))
-# creating list of cv scores
-cv_scores = []
-
-# perform 10-fold cross validation
-# better than train_test_split since the data set is smaller
-for k in k_list:
-    knn = KNeighborsClassifier(n_neighbors=k)
-    scores = cross_val_score(knn, X_train, y_train, cv=10, scoring='accuracy')
-    cv_scores.append(scores.mean())
-
-# changing to misclassification error
-MSE = [1 - x for x in cv_scores]
-
-plt.figure()
-plt.figure(figsize=(15,10))
-plt.title('The optimal number of neighbors', fontsize=20, fontweight='bold')
-plt.xlabel('Number of Neighbors K', fontsize=15)
-plt.ylabel('Misclassification Error', fontsize=15)
-sns.set_style("whitegrid")
-plt.plot(k_list, MSE)
-
-# plt.show()
-
-# finding best k
-best_k = k_list[MSE.index(min(MSE))]
-print("The optimal number of neighbors is %d." % best_k)
+plt.show()
